3a4aee343a5a2f0ee06c9540d92546072605e75b
Geison Correia <geisonthcorreia@gmail.com>
2020-04-17 18:51:17 -0300

    Spring boot client using SpringTemplate

---
+++ src/main/java/com/example/msscbreweryclient/web/client/BreweryClient.java
@@ -0,0 +1,31 @@
+package com.example.msscbreweryclient.web.client;
+
+import com.example.msscbreweryclient.web.model.BeerDto;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.UUID;
+
+@Component
+@ConfigurationProperties(value = "sfg.brewery", ignoreInvalidFields = false)
+public class BreweryClient {
+
+    public final String BEER_PATH_V1 = "/api/v1/beer/";
+
+    private String apihost;
+    private final RestTemplate restTemplate;
+
+    public BreweryClient(RestTemplateBuilder restTemplateBuilder) {
+        this.restTemplate = restTemplateBuilder.build();
+    }
+
+    public BeerDto getById(UUID uuid) {
+        return restTemplate.getForObject(apihost + BEER_PATH_V1 + uuid.toString(), BeerDto.class);
+    }
+
+    public void setApihost(String apihost) {
+        this.apihost = apihost;
+    }
+}
---
+++ src/test/java/com/example/msscbreweryclient/web/client/BreweryClientTest.java
@@ -0,0 +1,23 @@
+package com.example.msscbreweryclient.web.client;
+
+import com.example.msscbreweryclient.web.model.BeerDto;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class BreweryClientTest {
+
+    @Autowired
+    BreweryClient client;
+
+    @Test
+    void getById() {
+        BeerDto beerDto = client.getById(UUID.randomUUID());
+        assertNotNull(beerDto);
+    }
+}
